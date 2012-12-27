efa9e4d51725e9d0520805ebf1cab94cccf8130e
Brian Bockelman <bbockelm@cse.unl.edu>
2012-12-27 13:08:57 -0600

    WIP - add support for querying jobs.

--- src/condor.cpp
+++ src/condor.cpp
@@ -6,18 +6,49 @@
 
 using namespace boost::python;
 
-object queryCollector(const std::string &pool, const std::string &constraint="", const std::vector<std::string> *attrs=NULL)
+#define PARSE_COLLECTOR_QUERY_ERRORS(result) \
+    switch (result) { \
+    case Q_OK: \
+        break; \
+    case Q_INVALID_CATEGORY: \
+        PyErr_SetString(PyExc_RuntimeError, "Category not supported by query type."); \
+        boost::python::throw_error_already_set(); \
+    case Q_MEMORY_ERROR: \
+        PyErr_SetString(PyExc_MemoryError, "Memory allocation error."); \
+        boost::python::throw_error_already_set(); \
+    case Q_PARSE_ERROR: \
+        PyErr_SetString(PyExc_SyntaxError, "Query constraints could not be parsed."); \
+        boost::python::throw_error_already_set(); \
+    case Q_COMMUNICATION_ERROR: \
+        PyErr_SetString(PyExc_IOError, "Failed communication with collector."); \
+        boost::python::throw_error_already_set(); \
+    case Q_INVALID_QUERY: \
+        PyErr_SetString(PyExc_RuntimeError, "Invalid query."); \
+        boost::python::throw_error_already_set(); \
+    case Q_NO_COLLECTOR_HOST: \
+        PyErr_SetString(PyExc_RuntimeError, "Unable to determine collector host."); \
+        boost::python::throw_error_already_set(); \
+    default: \
+        PyErr_SetString(PyExc_RuntimeError, "Unknown error from collector query."); \
+        boost::python::throw_error_already_set(); \
+    } \
+
+
+object queryCollector(const std::string &pool, const std::string &constraint="", list attrs=list)
 {
     CondorQuery query(ANY_AD);
     if (constraint.length())
     {
         query.addANDConstraint(constraint.c_str());
     }
-    if (attrs && attrs->size())
+    if (attrs->size())
     {
+        std::vector<std::string> attrs_str; attrs_str.reserve(attrs.size());
+        for (int i=0; i<attrs.size(); i++)
+            attrs_str[i] = extract<std::string>(attrs[i]);
         std::vector<const char *> attrs_char; attrs_char.reserve(attrs->size()+1);
         attrs_char[attrs->size()] = NULL;
-        for (std::vector<std::string>::const_iterator it=attrs->begin(); it!=attrs->end(); it++)
+        for (std::vector<std::string>::const_iterator it=attrs_str->begin(); it!=attrs->end(); it++)
             attrs_char[0] = it->c_str();
         query.setDesiredAttrs(&attrs_char[0]);
     }
@@ -25,31 +56,7 @@ object queryCollector(const std::string &pool, const std::string &constraint="",
     //CondorError errStack;
     QueryResult result = query.fetchAds(adList, pool.c_str(), NULL);
 
-    switch (result) {
-    case Q_OK:
-        break;
-    case Q_INVALID_CATEGORY:
-        PyErr_SetString(PyExc_RuntimeError, "Category not supported by query type.");
-        boost::python::throw_error_already_set();
-    case Q_MEMORY_ERROR:
-        PyErr_SetString(PyExc_MemoryError, "Memory allocation error.");
-        boost::python::throw_error_already_set();
-    case Q_PARSE_ERROR:
-        PyErr_SetString(PyExc_SyntaxError, "Query constraints could not be parsed.");
-        boost::python::throw_error_already_set();
-    case Q_COMMUNICATION_ERROR:
-        PyErr_SetString(PyExc_IOError, "Failed communication with collector.");
-        boost::python::throw_error_already_set();
-    case Q_INVALID_QUERY:
-        PyErr_SetString(PyExc_RuntimeError, "Invalid query.");
-        boost::python::throw_error_already_set();
-    case Q_NO_COLLECTOR_HOST:
-        PyErr_SetString(PyExc_RuntimeError, "Unable to determine collector host.");
-        boost::python::throw_error_already_set();
-    default:
-        PyErr_SetString(PyExc_RuntimeError, "Unknown error from collector query.");
-        boost::python::throw_error_already_set();
-    }
+    PARSE_COLLECTOR_QUERY_ERRORS(result);
 
     list retval;
     ClassAd * ad;
@@ -65,9 +72,166 @@ object queryCollector(const std::string &pool, const std::string &constraint="",
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(queryCollector_overloads, queryCollector, 1, 3);
 
+struct JobQuery {
+
+    object run(classad::ClassAd scheddLocation, const std::string &constraint="", list attrs=list())
+    {
+        CondorQ q;
+
+        if (constraint.size())
+            q.addAND(constraint.c_str());
+
+        std::string scheddAddr, scheddName, scheddMachine, scheddVersion;
+        if ( ! (ad->EvaluateAttrString(ATTR_SCHEDD_IP_ADDR, scheddAddr)  &&
+                ad->EvaluateAttrString(ATTR_NAME, scheddName) &&
+                ad->EvaluateAttrString(ATTR_VERSION, scheddVersion) ) )
+        {
+            PyErr_SetString(PyExc_RuntimeError, "Unable to locate schedd address.");
+            throw_error_already_set();
+        }
+
+        StringList attrs_list;
+        for (int i=0, i<len(attrs), i++):
+            attrs_list.append(extract<std::string>(attrs[i]));
+
+        ClassAdList jobs;
+
+        int fetchResult = q.fetchQueueFromHost(jobs, attrs_list, scheddAddress, scheddVersion, NULL)
+        switch (fetchResult)
+        {
+        case Q_OK:
+            break;
+        case Q_PARSE_ERROR:
+        case Q_INVALID_CATEGORY:
+            PyErr_SetString(PyExc_RuntimeError, "Parse error in constraint.");
+            throw_error_already_set();
+            break;
+        default:
+            PyErr_SetString(PyExc_IOError, "Failed to fetch ads from schedd.");
+            throw_error_already_set();
+            break;
+        }
+
+        list retval;
+        ClassAd *job;
+        jobs.Open();
+        while ((job = jobs.Next()))
+        {
+            boost::shared_ptr<ClassAdWrapper> wrapper(new ClassAdWrapper());
+            wrapper->CopyFrom(*job);
+            retval.append(wrapper);
+        }
+    }
+
+    ClassAdWrapper *locateLocal()
+    {
+        Daemon schedd( DT_SCHEDD, 0, 0 );
+
+        ClassAdWrapper *wrapper = new ClassAdWrapper();
+        if (schedd.locate())
+        {
+            classad::ClassAd *daemonAd;
+            if ((daemonAd = wrapper->daemonAd()))
+            {
+                wrapper.CopyFrom(daemonAd);
+            }
+            else
+            {
+                std::string addr = schedd.addr()
+                if (!schedd.addr() || !wrapper.InsertAttr(ATTR_SCHEDD_IP_ADDR, addr))
+                {
+                    PyErr_SetString(PyExc_RuntimeError, "Unable to locate schedd address.");
+                    throw_error_already_set();
+                }
+                std::string name = schedd.name() ? schedd.name() : "Unknown";
+                if (!wrapper.InsertAttr(ATTR_NAME, name))
+                {
+                    PyErr_SetString(PyExc_RuntimeError, "Unable to insert schedd name.");
+                    throw_error_already_set();
+                }
+                std::string hostname = schedd.fullHostname() ? schedd.fullHostname() : "Unknown";
+                if (!wrapper.InsertAttr(ATTR_MACHINE, hostname))
+                {
+                    PyErr_SetString(PyExc_RuntimeError, "Unable to insert schedd hostname.");
+                    throw_error_already_set();
+                }
+                std::string version = schedd.version() ? schedd.version() : "";
+                if (!wrapper.InsertAttr(ATTR_VERSION, version))
+                {
+                    PyErr_SetString(PyExc_RuntimeError, "Unable to insert schedd version.");
+                    throw_error_already_set();
+                }
+            }
+        }
+        else
+        {
+            PyErr_SetString(PyExc_RuntimeError, "Unable to locate local schedd");
+            boost::python::throw_error_already_set();
+        }
+        return wrapper;
+    }
+
+    object locate(const std::string & pool, const std::string &name)
+    {
+        std::string constraint = ATTR_NAME " == '" + name + "'";
+        list = locateConstrained(pool, constraint);
+        if (len(list) >= 1) {
+            return list[0];
+        }
+        PyErr_SetString(PyExc_ValueError, "Unable to find schedd.");
+        throw_error_already_set();
+        return object;
+    }
+
+    list locateAll(const std::string & pool)
+    {
+        std::string constraint = ATTR_TOTAL_JOB_ADS " > 0";
+        return locateConstrained(pool, constraint);
+    }
+
+    list locateConstrained(const std::string &pool, const std::string &constraint)
+    {
+        CondorQuery scheddQuery(SCHEDD_AD);
+        result = scheddQuery.addANDConstraint( constraint.c_str() );
+        if (result != Q_OK) {
+            PyErr_SetString(PyExc_ValueError, "Unable to add trivial constraint.");
+            throw_error_already_set();
+        }
+
+        CollectorList Collectors;
+        Collectors->append(new DCCollector(pool.c_str()));
+
+        CondorQuery scheddQuery(SCHEDD_AD);
+        ClassAdList scheddList;
+        QueryResult result = Collectors->query(scheddQuery, scheddList);
+
+        PARSE_COLLECTOR_QUERY_ERRORS(result);
+
+        list retval;
+        ClassAd * schedd;
+        scheddList.Open();
+        while ((ad = scheddList.Next()))
+        {
+            boost::shared_ptr<ClassAdWrapper> wrapper(new ClassAdWrapper());
+            wrapper->CopyFrom(*schedd);
+            retval.append(wrapper);
+        }
+        return retval;
+    }
+}
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(JobQuery_run_overloads, &JobQuery::run, 1, 3);
+
 BOOST_PYTHON_MODULE(condor)
 {
     import("classad");
 
     def("query_collector", queryCollector, queryCollector_overloads());
+
+    class_<JobQuery>("JobQuery")
+        .def("run", &JobQuery::run, JobQuery_run_overloads())
+        .def("locateLocal", &JobQuery::locateLocal)
+        .def("locate", &JobQuery::locate)
+        .def("locateAll", &JobQuery::locateAll)
+        ;
 }
